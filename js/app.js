f564899c66b69131827f9af2b81b9d358a4f11ea
Unexperiance1student <75188379+Unexperiance1student@users.noreply.github.com>
2022-09-06 09:22:20 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// 3D scroll
+
+let zSpacing = -1000,
+    lastPos = zSpacing / 5
+    $frames = document.getElementsByClassName('frame'),
+    frames = Array.from($frames),
+    zVals = []
+
+window.onscroll = function(){
+    let top = document.documentElement.scrollTop,
+        delta = lastPos - top
+    
+    lastPos = top
+
+    frames.forEach((n, index) => {
+        zVals.push((index * zSpacing) + zSpacing)
+        zVals[index] += delta * -5.5
+        let frame = frames[index],
+            transform = `translateZ(${zVals[index]}px)`
+            opacity = zVals[index] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+        frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+    });
+}
+
+window.scrollTo(0, 1)
+
+// Audio
+let soundBtn = document.querySelector('.soundbutton'),
+    audio = document.querySelector('.audio')
+
+soundBtn.addEventListener('click', ()=>{
+    soundBtn.classList.toggle('paused')
+    audio.paused ? audio.play() :  audio.pause()
+})
+
+window.onfocus = ()=>{
+    soundBtn.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = ()=>{
+    audio.pause()
+}
